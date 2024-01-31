2fcb404608271ced0992416ae23f0ae8cf1fd074
KrAkashdeep <akashdeep93140@gmail.com>
2024-01-31 21:30:26 +0530

    fetch weather data

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ search.addEventListener("click", () => {
     .then((res) => res.json())
     .then((json) => {
       const image = document.querySelector(".weather-box img");
-      const temperature = document.querySelector(".weather-box temperature");
-      const description = document.querySelector(".weather-box description");
+      const temperature = document.querySelector(".weather-box .temperature");
+      const description = document.querySelector(".weather-box .description");
       const humidity = document.querySelector(
         ".weather-details .humidity span"
       );
